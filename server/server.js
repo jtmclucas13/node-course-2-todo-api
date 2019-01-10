6fd891155ee183627ba0dca9d35d71dfa9436764
jtmclucas13 <jtmclucas13@gmail.com>
2019-01-09 23:34:37 -0500

    Attempt to resolve heroku mongoose connection issues

--- server/server.js
+++ server/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
 
+const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
