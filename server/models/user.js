bd168b786f961caab482f6242e11811b4c31d354
jtmclucas13 <jtmclucas13@gmail.com>
2019-01-26 14:22:59 -0500

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -51,6 +51,16 @@ UserSchema.methods.generateAuthToken = function() {
     });
 };
 
+UserSchema.methods.removeToken = function(token) {
+    const user = this;
+
+    return user.update({
+        $pull: {
+            tokens: { token },
+        },
+    })
+};
+
 UserSchema.statics.findByCredentials = function(email, password) {
     const User = this;
 
