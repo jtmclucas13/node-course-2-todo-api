eed522ff6ab3f411fad72b5a3b55cba189f16cec
jtmclucas13 <jtmclucas13@gmail.com>
2019-01-27 20:17:39 -0500

    Upgrade expect to version 21.1.0

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -121,7 +121,7 @@ describe('DELETE /todos/:id', () => {
             .set('x-auth', users[1].tokens[0].token)
             .expect(200)
             .expect((res) => {
-                expect(res.body.todo._id).toBe(hexId)
+                expect(res.body.todo._id).toBe(hexId);
             })
             .end((err, res) => {
                 if (err) {
@@ -130,7 +130,7 @@ describe('DELETE /todos/:id', () => {
 
                 Todo.findById(hexId)
                     .then((todo) => {
-                        expect(todo).toNotExist();
+                        expect(todo).toBeFalsy();
                         done();
                     })
                     .catch((err) => done(err));
@@ -150,7 +150,7 @@ describe('DELETE /todos/:id', () => {
 
                 Todo.findById(hexId)
                     .then((todo) => {
-                        expect(todo).toExist();
+                        expect(todo).toBeTruthy();
                         done();
                     })
                     .catch((err) => done(err));
@@ -187,7 +187,7 @@ describe('PATCH /todos/:id', () => {
             .expect((res) => {
                 expect(res.body.todo.text).toBe(text);
                 expect(res.body.todo.completed).toBe(true);
-                expect(res.body.todo.completedAt).toBeA('number');
+                expect(typeof res.body.todo.completedAt).toBe('number');
             })
             .end(done);
     });
@@ -214,7 +214,7 @@ describe('PATCH /todos/:id', () => {
             .expect((res) => {
                 expect(res.body.todo.text).toBe(text);
                 expect(res.body.todo.completed).toBe(false);
-                expect(res.body.todo.completedAt).toNotExist();
+                expect(res.body.todo.completedAt).toBeFalsy();
             })
             .end(done);
     });
@@ -254,8 +254,8 @@ describe('POST /users', () => {
             .send({ email, password })
             .expect(200)
             .expect((res) => {
-                expect(res.headers['x-auth']).toExist();
-                expect(res.body._id).toExist();
+                expect(res.headers['x-auth']).toBeTruthy();
+                expect(res.body._id).toBeTruthy();
                 expect(res.body.email).toBe(email);
             })
             .end((err) => {
@@ -264,8 +264,8 @@ describe('POST /users', () => {
                 }
 
                 User.findOne({ email }).then((user) => {
-                    expect(user).toExist();
-                    expect(user.password).toNotBe(password);
+                    expect(user).toBeTruthy();
+                    expect(user.password).not.toBe(password);
                     done();
                 })
                 .catch((e) => done(e));
@@ -296,7 +296,7 @@ describe('POST /users/login', () => {
             .send({ email: users[1].email, password: users[1].password })
             .expect(200)
             .expect((res) => {
-                expect(res.headers['x-auth']).toExist();
+                expect(res.headers['x-auth']).toBeTruthy();
             })
             .end((err, res) => {
                 if (err) {
@@ -304,7 +304,7 @@ describe('POST /users/login', () => {
                 }
 
                 User.findById(users[1]._id).then((user) => {
-                    expect(user.tokens[1]).toInclude({
+                    expect(user.toObject().tokens[1]).toMatchObject({
                         access: 'auth',
                         token: res.headers['x-auth'],
                     });
@@ -320,7 +320,7 @@ describe('POST /users/login', () => {
             .send({ email: users[1].email, password: 'invalid-password' })
             .expect(400)
             .expect((res) => {
-                expect(res.headers['x-auth']).toNotExist();
+                expect(res.headers['x-auth']).toBeFalsy();
             })
             .end((err, res) => {
                 if (err) {
